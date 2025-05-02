3e736dd0b770fc8186fa60f5217ddfe0aae7d1ae
Weslley Gomes <165548328+Uejii@users.noreply.github.com>
2025-05-02 14:23:06 -0300

    Atualizado weslley 2/maio

--- projeto.py
+++ projeto.py
@@ -5,3 +5,9 @@ def main():
     peso = obter_valor_numerico("Digite seu peso (em kg): ")
     altura = obter_valor_numerico("Digite sua altura (em metros): ")
     imc = calcular_imc(peso, altura)
+#calculadora imc
+print("=== Calculadora de IMC ===") 
+peso=float(input("Digite seu peso em KG"))
+altura=float(input("Digite sua altura em metros"))
+imc= peso/(altura*altura)
+print ("seu imc é,", imc)
