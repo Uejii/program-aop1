081243ae741c7b6f8f2f431f9e81e01511f3c13f
Weslley Gomes <165548328+Uejii@users.noreply.github.com>
2025-05-03 02:19:27 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -4,4 +4,4 @@ peso=float(input("Digite seu peso em KG"))
 altura=float(input("Digite sua altura em metros"))
 imc= peso/(altura*altura)
 print ("seu imc é,", imc)
-#teste de envio
+#consegui apagar seu codigo? nao sei mas to testando o push aqui bjs
