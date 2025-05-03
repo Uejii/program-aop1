4ed73a728cf4544f0cb7ec8d6304dc9a130dcaa7
Weslley Gomes <165548328+Uejii@users.noreply.github.com>
2025-05-03 01:42:39 -0300

    git commit

--- projeto.py
+++ projeto.py
@@ -1,10 +1,3 @@
-def main():
-    
-    print("=== Calculadora de IMC ===")
-
-    peso = obter_valor_numerico("Digite seu peso (em kg): ")
-    altura = obter_valor_numerico("Digite sua altura (em metros): ")
-    imc = calcular_imc(peso, altura)
 #calculadora imc
 print("=== Calculadora de IMC ===") 
 peso=float(input("Digite seu peso em KG"))
