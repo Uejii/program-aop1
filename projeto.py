83baf4779e541c15197fb2a86cd639ee81ab0433
Weslley Gomes <165548328+Uejii@users.noreply.github.com>
2025-05-05 17:47:18 -0300

    Atualizar o projeto.py

--- projeto.py
+++ projeto.py
@@ -1,28 +1,29 @@
 #calculadora imc jeofton 
 print("=== Calculadora de IMC ===")
-while True :
-  try:
-  peso = float(input("Digite seu peso em KG "))
-  altura = float(input("Digite sua altura em metros "))
-  imc = peso/(altura*altura)
-  print("Seu IMC é:", round(imc, 2))
-  
-  if imc < 18.5:
+while True:
+    try:
+        peso = float(input("Digite seu peso em KG "))
+        altura = float(input("Digite sua altura em metros "))
+        imc = peso / (altura * altura)
+        print("Seu IMC é:", round(imc, 2))
+        
+        if imc < 18.5:
             print("Classificação: Abaixo do peso")
-  elif imc < 25:
+        elif imc < 25:
             print("Classificação: Peso normal")
-  elif imc < 30:
+        elif imc < 30:
             print("Classificação: Sobrepeso")
-  elif imc < 35:
+        elif imc < 35:
             print("Classificação: Obesidade grau 1")
-  elif imc < 40:
+        elif imc < 40:
             print("Classificação: Obesidade grau 2")
-   else:
+        else:
             print("Classificação: Obesidade grau 3")
-except ValueError:
+    except ValueError:
         print("Por favor, digite um número válido.")
         continue
-  repetir= input("Deseja calcular novamente? S/N  ")
-  if repetir != "s" :
-     print("Encerrando a aplicaçao, ate mais")
-     break
+    
+    repetir = input("Deseja calcular novamente? S/N  ")
+    if repetir.lower() != "s":
+        print("Encerrando a aplicação, até mais")
+        break
