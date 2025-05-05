0572ab1545ff6bf531544520d6f611325192e50f
Weslley Gomes <165548328+Uejii@users.noreply.github.com>
2025-05-05 12:54:44 -0300

    Adicionei o while

--- projeto.py
+++ projeto.py
@@ -1,6 +1,11 @@
 #calculadora imc
-print("=== Calculadora de IMC ===") 
-peso=float(input("Digite seu peso em KG"))
-altura=float(input("Digite sua altura em metros"))
-imc= peso/(altura*altura)
-print ("seu imc é,", imc)
+print("=== Calculadora de IMC ===")
+while True :
+ peso = float(input("Digite seu peso em KG "))
+ altura = float(input("Digite sua altura em metros "))
+ imc = peso/(altura*altura)
+ print("Seu IMC é:", round(imc, 2))
+ repetir= input("Deseja calcular novamente? S/N  ")
+ if repetir != "s" :
+    print("Encerrando a aplicaçao, ate mais")
+    break
